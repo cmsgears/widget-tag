adb65ef6f772904a8dd5dd49b37fec24140fc31f
Bhagwat Chouhan <bhagwat.chouhan@gmail.com>
2017-05-12 12:40:18 +0530

    Fixed tag mapper issue.

--- views/templates/tag.php
+++ views/templates/tag.php
@@ -3,7 +3,7 @@
 	{{#each tags }}
 	<span class='tag'>
 		<span class="text">{{name}}</span>
-		<span id="frm-delete-tag-{{slug}}" app="main" cmt-controller="tag" cmt-action="delete" action="<?= $removeUrl ?>&tag-slug={{slug}}" method="post">
+		<span id="frm-delete-tag-{{slug}}" cmt-app="main" cmt-controller="tag" cmt-action="delete" action="<?= $removeUrl ?>&tag-slug={{slug}}" method="post">
 			<div class="max-area-cover spinner">
 				<div class="valign-center cmti cmti-spinner-1 spin"></div>
 			</div>
